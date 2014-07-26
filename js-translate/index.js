0e1f1bceb97628760ddfa74982a3d677a0a4943e
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-26 17:00:17 +0600

    WIP3

--- js-translate/index.js
+++ js-translate/index.js
@@ -3,5 +3,5 @@ var gcc = new (require('./lib/gcc.js'))();
 
 module.exports = function complie(code) {
 	var tree = esprima.parse(code);
-	return gcc.translate(tree.body);
+	return gcc.translate(tree.body[0]);
 };
